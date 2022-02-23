238f180938143441091956c94ca4af67caf9b1d9
Sathavara <bhavesh.sathavara@gamcustom.com>
2022-02-22 16:10:12 -0800

    Update User Info API.ts

--- Test Suites/API Test suites/User Info API.ts
+++ Test Suites/API Test suites/User Info API.ts
@@ -12,16 +12,16 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>5b04e277-0e1e-4bf7-bb61-edd9a4603db2</testSuiteGuid>
    <testCaseLink>
-      <guid>65c60c92-f093-4c6f-a4c8-d21b94e3fd43</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/API Tests/User Info API Test/GIven Test Data userName test case from excel</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
       <guid>0b7f4639-8f24-4988-a42d-1a067201d499</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/API Tests/User Info API Test/Verify InValid Scenarios - User Info API</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>65c60c92-f093-4c6f-a4c8-d21b94e3fd43</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Tests/User Info API Test/GIven Test Data userName test case from excel</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>67b72765-52f2-4bcc-afaa-962af5bd7c1d</guid>
